cb04ea5b0dc89e9dcf4be1aa1d9609ec87bb8119
u20159 <vitoriosodez@gmail.com>
2022-09-12 12:52:09 -0300

    Implementação de select geral das matérias

--- rotas.js
+++ rotas.js
@@ -1,7 +1,22 @@
-const usuario = '/usuario/'
+const usuario = '/usuario'
+const materia = '/materia'
 
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.status(200).send("success");
+module.exports = (app, db) => {
+    app.get(materia, (req, res) => {
+        const sql = 'SELECT * FROM MATERIA';
+        db.query(sql, (err, qres) => {
+            if (err)
+            {
+                res.status(400).send("Bad Request");
+                return;
+            }
+
+            if (qres.rowCount == 0) {
+                res.status(404).send("Not Found");
+                return;
+            }
+
+            res.status(200).json(qres);
+        })
     })
 }

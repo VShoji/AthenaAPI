910938a78ab49f908255f28c89390a5a44472f3e
VShoji <69880007+VShoji@users.noreply.github.com>
2022-09-27 23:21:11 -0300

    Organizing codebase

--- bd.js
+++ bd.js
@@ -1,29 +1,20 @@
 require('dotenv').config()
-const {Client}    = require("pg");
 
-async function getConexao ()
-{
-    const client = new Client({
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        password: process.env.PASSWORD,
-        port: process.env.port,
-        ssl:{
-            rejectUnauthorized: false
-        }
-    });
-
-    try
-    {
-        client.connect();
-        return client;
+const {Client} = require("pg");
+const client = new Client({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.port,
+    ssl:{
+        rejectUnauthorized: false
     }
-    catch (erro)
-    {
-        console.log("> Error while trying to connect: " + erro);
-        return null;
-    }
-}
+});
 
-module.exports = {getConexao}
+client.connect(err => {
+    if (err)
+        console.log('> ' + err)
+});
+
+module.exports = client;
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const app = require('express')();
-
-createDbConnection()
-
 const port = 3000
 
 // Logger middleware
 app.use(require('./middleware/log.js'))
 
-app.get('/', (req, res) => {
-    res.status(200).send("Success")
-})
+// Routes
+app.use('/materia', require('./routes/materia.js'))
 
 app.listen(port, () => {
     console.log("Server listening to port: " + port);
 })
-
-async function createDbConnection() {
-    const db = require('./bd');
-    const con = await db.getConexao();
-
-    if (!con) {
-        console.log('Failed to establish database connection');
-        process.exit(-1);
-    }
-}

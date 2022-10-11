08bf191aea414a214b9ffa3d5c99a265f7aa0285
VShoji <69880007+VShoji@users.noreply.github.com>
2022-10-10 23:23:13 -0300

    not tested

--- routes/authUser.js
+++ routes/authUser.js
@@ -8,15 +8,15 @@ const router = require('express').Router();
 // Cadastro
 
 router.post('/cadastro', (req, res) => {
-    bcrypt.hash(req.body.senhaUsuario, 10, (err, hashPassword) => {
+    bcrypt.hash(req.body.senhaUsuario, 10, (err, hash) => {
         if (err) {
             console.log("> " + err)
             res.status(500).send('Internal Server Error');
             return;
         }
 
-        const query = "INSERT INTO USUARIO(idUsuario, nomeUsuario, emailUsuario, senhaUsuario) VALUES (DEFAULT, '$1', '$2', '$3')";
-        const values = [req.body.nomeUsuario, req.body.emailUsuario, hashPassword];
+        const query = "INSERT INTO USUARIO(idUsuario, nomeUsuario, emailUsuario, senhaUsuario) VALUES (DEFAULT, $1, $2, $3)";
+        const values = [req.body.nomeUsuario, req.body.emailUsuario, hash];
     
         db.query(query, values, (err, data) => {
             if (err) {
@@ -33,29 +33,36 @@ router.post('/cadastro', (req, res) => {
 // login
 
 router.post('/login', (req, res) => {
+    const query = "SELECT * FROM Usuario WHERE emailUsuario = $1";
+    const values = [req.body.emailUsuario];
 
-/*
-    let user;
-    user = await UsuarioController.getUserByEmail(req.body.emailUsuario);
-
-    if (user == undefined) {
-        return res.status(404).send("Not found");
-    }
-
-    try {
-        const match =  bcrypt.compare(req.body.senhaUsuario, user.senhausuario);
-        const accessToken = jwt.sign(JSON.stringify(user), process.env.TOKEN_SECRET);
-        if (match) {
-            res.json({ accessToken: accessToken });
-
-        } else {
-            res.json({ message: "Invalid Credentials" });
+    db.query(query, values, (err, data) => {
+        if (err) {
+            console.log("> " + err)
+            res.status(500).send('Internal Server Error');
+            return;
         }
-    }
-    catch (e) {
-        console.log(e);
-    }
-*/
+
+        if (data.rowCount == 0){
+            res.status(404).send('Not Found');
+            return;
+        }
+
+        bcrypt.compare(req.body.senhaUsuario, data.rows[0].senhaUsuario, (err, same) => {
+            if (err) {
+                console.log("> " + err)
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+
+            if (!same) {
+                res.status(403).send("Access denied");
+            }
+
+            const tok = jwt.sign(JSON.stringify(data[0], process.env.TOKEN_SECRET));
+            res.status(200).send(tok);
+        })
+    })
 })
 
 module.exports = router;
